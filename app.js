7b0560a896d03a81c80c715a6a8c3545fee2405f
Jessica Huynh <huynhjessica19@gmail.com>
2019-06-17 08:20:54 -0700

    psuedo code

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-// var firebaseConfig = {
-//   apiKey: "AIzaSyAmB5TI4x4s0X0KwqOlY8_FHsXofXd5mDQ",
-//   authDomain: "choochoo-dff3e.firebaseapp.com",
-//   databaseURL: "https://choochoo-dff3e.firebaseio.com",
-//   projectId: "choochoo-dff3e",
-//   storageBucket: "choochoo-dff3e.appspot.com",
-//   messagingSenderId: "373847630827",
-//   appId: "1:373847630827:web:b45a51f89312ee00"
-// };
+//I was able to link my firebase, but the data that would show up all came back undefined 
 var firebaseConfig = {
   apiKey: "AIzaSyCq5Fo3cCTxNmm5z2MTtWlu2qU2lv3L-tg",
   authDomain: "train-scheduler-1d159.firebaseapp.com",
@@ -19,67 +11,62 @@ var firebaseConfig = {
 
 
   
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
-  
-  //Declaring the current time
+var database = firebase.database();
+
 var currentTime = moment().format();
 
-//Logging the current time
+
 	console.log("Current Time: " + currentTime);
-				
-//When the submit button is clicked, we will run the snapshot function below.
+
 $("#click-button").on("click", function() {
-      // Prevent the page from refreshing
+
       event.preventDefault();
 
-      // Grabs user input
 	  var trainNameForm = $("#trainNameForm").val().trim();
 	  var destinationForm = $("#destinationForm").val().trim();
 	  var trainTimeForm = moment($("#trainTimeForm").val().trim(), "HH:mm").format("HH:mm");
-	//Saving this goodness
-//	  var frequencyForm = moment($("#frequencyForm").val().trim().format("mm"));
+
 	  var frequencyForm = $("#frequencyForm").val().trim();
 
-	  // Creates local "temporary" object for holding inputs
 	  var newTrain = {
 		train: trainNameForm,
 		destination: destinationForm,
 		first: trainTimeForm,
 		frequency: frequencyForm
     };
-	//Setting the new values in the database
+	
 	database.ref().push(newTrain);
 	
-	//Console.logging to make sure the new data has been stored to the database
+
 	console.log(newTrain.train);
-  	console.log(newTrain.destination);
+  console.log(newTrain.destination);
 	console.log(newTrain.first);
 	console.log(newTrain.frequency);
 	
-	//Clearing the inputs
+
 	 $("#trainNameForm").val("");
-  	 $("#destinationForm").val("");
+  $("#destinationForm").val("");
 	 $("#trainTimeForm").val("");
 	 $("#frequencyForm").val("");
 });
 
-//Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
 	
-  // Store everything into a variable.
+  
   var trainName = childSnapshot.val().train;
   var trainDestination = childSnapshot.val().destination;
   var trainTime = childSnapshot.val().first;
   var trainFrequency = childSnapshot.val().frequency;
   
-  //Variable to figure out the converted train time
+  
   var trainTimeConverted = moment(trainTime, "HH:mm");
 	
-  //Declaring a time difference variable
+ 
   var timeDifference = moment().diff(moment(trainTimeConverted), "minutes");
 	console.log(timeDifference);
 	
@@ -93,6 +80,6 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	console.log("Next Arrival: " + nextArrival);
 	
 	
-  //Adding into the table
+  
   $("#trainScheduleTable > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 });
